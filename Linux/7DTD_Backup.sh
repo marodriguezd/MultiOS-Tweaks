50752f12152ae0cc9669480db0f9f7503ca05572
marodriguezd <migueadali@gmail.com>
2024-06-30 12:08:45 +0200

    Actualizado Script copia seguridad 7DTD

--- Linux/7DTD_Backup.sh
+++ Linux/7DTD_Backup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-cd /home/marodriguezd/Saves_links/7DTD_Saves
-tar -czvf 7DTD_Saves_Backup_Day_X.tar.gz .
-mv 7DTD_Saves_Backup_Day_X.tar.gz /home/marodriguezd/Saves_links
+echo "Arrancando Script..."
+echo "Accediendo a ruta..."
+echo ""
+cd /home/marodrigued/.local/share/Steam/steamapps/compatdata/251570/pfx/drive_c/users/steamuser/AppData/Roaming/7DaysToDie/Saves
+echo "Comprimiendo Saves..."
+tar -czf 7DTD_Saves_Backup_Day_X.tar.gz . 2>/dev/null
+echo "Moviendo Saves a destino..."
+echo ""
+mv 7DTD_Saves_Backup_Day_X.tar.gz /home/marodrigued/SaveGames
+echo "Script completado."
