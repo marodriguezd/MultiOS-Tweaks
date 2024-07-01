ab7c21ff808a2d7ea230df11c8590aa4559f6205
marodriguezd <migueadali@gmail.com>
2024-07-01 20:16:44 +0200

    Mejorados y facilitada la edición del script de backup

--- Linux/7DTD_Backup.sh
+++ Linux/7DTD_Backup.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
+
+# Define el nombre base del comprimido final
+COMPRESSED_NAME=7DTD_Saves_Backup_Day_X
+
+# Define la ruta de los saves y la ruta de destino
+SAVE_DIR=~/.local/share/Steam/steamapps/compatdata/251570/pfx/drive_c/users/steamuser/AppData/Roaming/7DaysToDie/Saves
+DEST_DIR=~/SaveGames
+
+# Obtener la fecha actual en formato YYYYMMDD
+CURRENT_DATE=$(date +"%Y%m%d")
+
 echo "Arrancando Script..."
 echo "Accediendo a ruta..."
 echo ""
-cd ~/.local/share/Steam/steamapps/compatdata/251570/pfx/drive_c/users/steamuser/AppData/Roaming/7DaysToDie/Saves 2>/dev/null
+
+# Acceder a la ruta de los saves
+cd "$SAVE_DIR" 2>/dev/null
+
 echo "Comprimiendo Saves..."
-tar -czf 7DTD_Saves_Backup_Day_X.tar.gz . 2>/dev/null
+# Crear el archivo comprimido con la fecha actual en el nombre
+tar -czf "$COMPRESSED_NAME"_"$CURRENT_DATE".tar.gz . 2>/dev/null
+
 echo "Moviendo Saves a destino..."
 echo ""
-mv 7DTD_Saves_Backup_Day_X.tar.gz ~/SaveGames 2>/dev/null
+
+# Mover el archivo comprimido al destino
+mv "$COMPRESSED_NAME"_"$CURRENT_DATE".tar.gz "$DEST_DIR" 2>/dev/null
+
 echo "Script completado."
--- Linux/MX360J.sh
+++ Linux/MX360J.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
+
+# Define el nombre base del comprimido final
+COMPRESSED_NAME=MX360J
+
+# Define la ruta de los saves y la ruta de destino
+SAVE_DIR=~/.config/com.modrinth.theseus/profiles/Legacy\ Reborn/saves/
+DEST_DIR=~/SaveGames
+
+# Obtener la fecha actual en formato YYYYMMDD
+CURRENT_DATE=$(date +"%Y%m%d")
+
 echo "Arrancando Script..."
 echo "Accediendo a ruta..."
 echo ""
-cd ~/.config/com.modrinth.theseus/profiles/Legacy Reborn/saves/ 2>/dev/null
+
+# Acceder a la ruta de los saves
+cd "$SAVE_DIR" 2>/dev/null
+
 echo "Comprimiendo Saves..."
-tar -czf MX360J_Ver_X.tar.gz . 2>/dev/null
+# Crear el archivo comprimido con la fecha actual en el nombre
+tar -czf "$COMPRESSED_NAME"_"$CURRENT_DATE".tar.gz . 2>/dev/null
+
 echo "Moviendo Saves a destino..."
 echo ""
-mv MX360J_Ver_X.tar.gz ~/SaveGames 2>/dev/null
+
+# Mover el archivo comprimido al destino
+mv "$COMPRESSED_NAME"_"$CURRENT_DATE".tar.gz "$DEST_DIR" 2>/dev/null
+
 echo "Script completado."
