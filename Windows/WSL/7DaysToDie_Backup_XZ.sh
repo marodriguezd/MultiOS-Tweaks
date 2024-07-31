6096c475744943d8771c61b1937fe811f305505a
marodriguezd <migueadali@gmail.com>
2024-07-31 20:50:15 +0200

    Añadido script para hacer .tar.xz de 7 days

---
+++ Windows/WSL/7DaysToDie_Backup_XZ.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Define el nombre base del comprimido final
+COMPRESSED_NAME=7DTD_Saves_Backup_Day_X
+
+# Define la ruta de los saves y la ruta de destino
+SAVE_DIR=/mnt/c/Users/marodriguezd/AppData/Roaming/7DaysToDie/Saves
+DEST_DIR=/mnt/c/Users/marodriguezd/Documents/7DTD_Backups
+
+# Obtener la fecha actual en formato YYYYMMDD
+CURRENT_DATE=$(date +"%Y%m%d_%H.%M")
+
+echo "Arrancando Script..."
+echo "Accediendo a ruta..."
+echo ""
+
+# Acceder a la ruta de los saves
+cd "$SAVE_DIR" 2>/dev/null
+
+echo "Comprimiendo Saves..."
+# Crear el archivo comprimido con la fecha actual en el nombre
+tar -cJf "$COMPRESSED_NAME"_"$CURRENT_DATE".tar.xz . 2>/dev/null
+
+echo "Moviendo Saves a destino..."
+echo ""
+
+# Mover el archivo comprimido al destino
+mv "$COMPRESSED_NAME"_"$CURRENT_DATE".tar.xz "$DEST_DIR" 2>/dev/null
+
+echo "Script completado."
